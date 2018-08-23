22e87d3f5d3647ab846399d8df971dbe13f50099
Ivan Martianov <i@ivansky.ru>
2018-08-23 20:59:26 +0400

    Add idea of converter

--- src/resolvers/abstract-converter.ts
+++ src/resolvers/abstract-converter.ts
@@ -17,10 +17,30 @@ export abstract class AbstractConverter<T extends object> {
     }
 }
 
-export class BaseConverter<T extends object> extends AbstractConverter<T> {}
+export interface IConverter<T> {
+    toDbObject(entity: IEntity<T>): IDbEntity<T>;
+    toEntity(dbObject: IDbEntity<T>): IEntity<T>;
+}
 
-export function makeEntityConverter(idFields: string[]) {
-    export class BaseConverter<T extends object> extends AbstractConverter<T> {
+export function makeEntityConverter<T extends object>(idFields: string[]): IConverter<T> {
+    return {
+        toDbObject: (entity: IEntity<T>): IDbEntity<T> => {
+            const keys: Array<keyof IEntity<T>> = Object.keys(entity) as any;
 
-    }
+            keys.reduce<PartiIDbEntity<T>>(() => {
+
+            })
+
+            const dbObject = { ...(entity as any), _id: new ObjectId(entity.id) };
+            delete dbObject.id;
+
+            return dbObject;
+        },
+        toEntity: (dbObject: IDbEntity<T>): IEntity<T> => {
+            const entity = { ...(dbObject as any), id: dbObject._id.toHexString() };
+            delete entity._id;
+
+            return entity;
+        },
+    };
 }
